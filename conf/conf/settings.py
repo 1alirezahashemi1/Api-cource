269499453b20fc07bd12230dac8015d396f839c2
alirezahashemi <alirezahahsemi1129@gmail.com>
2022-05-31 05:00:17 -0700

    Tests Dj-rest-auth Endpoints

--- conf/conf/settings.py
+++ conf/conf/settings.py
@@ -150,3 +150,5 @@ REST_FRAMEWORK = {
 }
 
 SITE_ID = 1
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
--- conf/conf/urls.py
+++ conf/conf/urls.py
@@ -2,7 +2,10 @@ from django.contrib import admin
 from django.urls import path , include
 from blog. views import *
 from rest_framework.authtoken.views import ObtainAuthToken
-
+from dj_rest_auth.views import (
+    LoginView, LogoutView, PasswordChangeView, PasswordResetConfirmView,
+    PasswordResetView
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -12,6 +15,6 @@ urlpatterns = [
     path('api/rest-auth/', include('dj_rest_auth.urls')),
     path('dj-rest-auth/', include('dj_rest_auth.urls')),
     path('dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')),
-
+    path('api/rest-auth/password/reset/confirm/<uidb64>/<token>', PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
 
 ]
